327341cacba87b3677e833d085be20cd68607556
AntoineCheminPro <antoine.chemin.pro@outlook.fr>
2025-04-02 11:15:49 +0200

    pages crées

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,9 +5,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { TopicComponent } from './pages/topic/topic.component';
+import { PostCreateComponent } from './pages/posts/post-create/post-create.component';
+import { PostDetailComponent } from './pages/posts/post-detail/post-detail.component';
+import { PostListComponent } from './pages/posts/post-list/post-list.component';
+import { UserComponent } from './pages/user/user.component';
+import { TopicsComponent } from './pages/topics/topics.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent, RegisterComponent, TopicComponent, PostCreateComponent, PostDetailComponent, PostListComponent, UserComponent, TopicsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
